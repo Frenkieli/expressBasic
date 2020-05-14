70736fa9f8eaa3d92aa2a7a868e367b788f0818f
Frenkieli <49364993+Frenkiele@users.noreply.github.com>
2020-05-14 14:45:39 +0800

    bug: 轉成用webpack讀取typescript就錯
    目前gulp可以正常運動但是應該要都使用webpack因為在gulp也是使用browserify製作進入點來製作，因為都有進入點應該讓webpack來做就好="=

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const nodeExternals = require('webpack-node-externals');
 const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-module.exports = {
+const serverConfig = {
   target: 'node',
   devtool: 'eval-source-map',
   node: {
@@ -30,6 +30,51 @@ module.exports = {
       }
     ],
   },
+  // plugins: [
+  //   new CopyWebpackPlugin([
+  //     { from: 'src/client/views', to: 'views' },
+  //     { from: 'src/client/static', to: 'public' },
+  //   ])
+  // ],
+  optimization: {
+    minimize: true,
+  }
+}
+
+const clientConfig = {
+  target: 'web',
+  devtool: 'eval-source-map',
+  node: {
+    __dirname: false,
+    __filename: true,
+  },
+  externals: [nodeExternals()],
+  entry: {
+    // 'index': './src/index.js',
+    'public/javascripts/temibroad': './src/client/typescript/temibroad/temibroad.ts'
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].bundle.js',
+    libraryTarget: 'commonjs2',
+  },
+  module: {   //設定你的檔案選項
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ],
+  },
   plugins: [
     new CopyWebpackPlugin([
       { from: 'src/client/views', to: 'views' },
@@ -40,3 +85,7 @@ module.exports = {
     minimize: true,
   }
 }
+
+
+
+module.exports = [serverConfig, clientConfig];
